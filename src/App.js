168e53f913781ec9414a9a4ef40477a8f99d4859
naianti <87500932+naianti@users.noreply.github.com>
2022-01-09 18:00:48 -0400

    added api dictionary integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import logo from "./logo.png";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header text-center">Hello</header>
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Choose words wisely!</h1>
+        </header>
         <main>
+          <h2>Improve your vocabulary and pronunciation</h2>
           <Dictionary />
         </main>
         <footer>Code by Nai Antillano</footer>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
@@ -13,10 +20,27 @@ export default function Dictionary() {
   }
 
   return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
+    <div className="Dictionary align-items-center">
+      <section>
+        <form className="searcher" onSubmit={search}>
+          <div className="row">
+            <div className="col-6 col-md-4">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Type any word  "
+                autoComplete="off"
+                autoFocus="on"
+                onChange={handleKeywordChange}
+              />
+            </div>
+
+            <div className="col-6 col-md-2">
+              <input type="submit" className="button" value="Go!" />
+            </div>
+          </div>
+        </form>
+      </section>
     </div>
   );
 }
